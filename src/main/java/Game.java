55cca765370c2fd7245ba56bf4e863cc9161f961
Besrat Ghebrat <besratghebrat@Besrats-MacBook-Air.local>
2021-08-21 16:00:27 -0400

    Finished

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,8 +1,7 @@
-
 import java.util.ArrayList;
-        import java.util.Scanner;
-        import java.util.Random;
-        import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
 
 public class Game {
     public static void main(String[] args) {
@@ -20,55 +19,145 @@ public class Game {
         System.out.println("-------------------------");
         Scanner input = new Scanner(System.in);
         int x = input.nextInt();
+        boolean outOfGuesses = false;
+        String guessWord = "";
+        String guess = input.nextLine();
+        int guessCount = 0;
+        int guessLimit = 5;
         if (x != 4) {
         }
-  switch (x) {
-        case 1:
-        Random randCategory = new Random();
-        List<String> animalCategory = new ArrayList<String>();
-        animalCategory.add("Lion");
-        animalCategory.add("Elephant");
-        animalCategory.add("Giraffe");
-        int numberOfElements = 1;
-            System.out.println(animalCategory);
-        for (int i = 0; i < numberOfElements; i++) {
-            int randomIndex = randCategory.nextInt(animalCategory.size());
-            String guessWord = animalCategory.get(randomIndex);
-            break;
-        }
-        break;
-        case 2:
-        Random randCategory1 = new Random();
-        List<String> occupationCategory = new ArrayList<String>();
-        occupationCategory.add("Programmer");
-        occupationCategory.add("Lawyer");
-        occupationCategory.add("Doctor");
-        int numberOfElements1 = 1;
 
-        for (int i = 0; i < numberOfElements1; i++) {
-            int randomIndex = randCategory1.nextInt(occupationCategory.size());
-            String guessWord = occupationCategory.get(randomIndex);
-            break;
-        }
-        break;
-        case 3:
-        Random randCategory2 = new Random();
-        List<String> celebritiesCategory = new ArrayList<String>();
-        celebritiesCategory.add("Obama");
-        celebritiesCategory.add("Beyonce");
-        celebritiesCategory.add("Rihanna");
-        int numberOfElements2 = 1;
+        switch (x) {
+            case 1:
+                Random randCategory = new Random();
+                List<String> animalCategory = new ArrayList<String>();
+                animalCategory.add("Lion");
+                animalCategory.add("Elephant");
+                animalCategory.add("Giraffe");
+                int numberOfElements = 1;
+                for (int i = 0; i < numberOfElements; i++) {
+                    int randomIndex = randCategory.nextInt(animalCategory.size());
+                    guessWord = animalCategory.get(randomIndex);
 
-        for (int i = 0; i < numberOfElements2; i++) {
-            int randomIndex = randCategory2.nextInt(celebritiesCategory.size());
-            String guessWord = celebritiesCategory.get(randomIndex);
-            break;
+
+                    break;
+                }
+
+                while (!guess.equalsIgnoreCase(guessWord) && !outOfGuesses) {
+
+                    if (guessCount < guessLimit) {
+                        System.out.print("Enter a guess: ");
+                        guess = input.nextLine();
+                        guessCount++;
+
+                    }
+
+                    if (guessCount == 2) {
+                        System.out.println("Hint:" + guessWord.charAt(0));
+
+                    }
+
+                    if (guessCount == 5) {
+                        outOfGuesses = true;
+                        System.out.println("You lose!");
+
+                    }
+                    if(guess.equalsIgnoreCase(guessWord)){
+                        System.out.println("You win!");
+                    }
+                }
+
+
+                break;
+
+
+            case 2:
+                Random randCategory1 = new Random();
+                List<String> occupationCategory = new ArrayList<String>();
+                occupationCategory.add("Programmer");
+                occupationCategory.add("Lawyer");
+                occupationCategory.add("Doctor");
+                int numberOfElements1 = 1;
+
+                for (int i = 0; i < numberOfElements1; i++) {
+                    int randomIndex = randCategory1.nextInt(occupationCategory.size());
+                    guessWord = occupationCategory.get(randomIndex);
+                    break;
+                }
+
+                while (!guess.equalsIgnoreCase(guessWord) && !outOfGuesses) {
+
+                    if (guessCount < guessLimit) {
+                        System.out.print("Enter a guess: ");
+                        guess = input.nextLine();
+                        guessCount++;
+
+                    }
+
+                    if (guessCount == 2) {
+                        System.out.println("Hint:" + guessWord.charAt(0));
+
+                    }
+
+                    if (guessCount == 5) {
+                        outOfGuesses = true;
+                        System.out.println("You lose!");
+
+                    }
+                    if(guess.equalsIgnoreCase(guessWord)){
+                        System.out.println("You win!");
+
+                    }
+                }
+
+
+                break;
+            case 3:
+                Random randCategory2 = new Random();
+                List<String> celebritiesCategory = new ArrayList<String>();
+                celebritiesCategory.add("Obama");
+                celebritiesCategory.add("Beyonce");
+                celebritiesCategory.add("Rihanna");
+                int numberOfElements2 = 1;
+
+                for (int i = 0; i < numberOfElements2; i++) {
+                    int randomIndex = randCategory2.nextInt(celebritiesCategory.size());
+                    guessWord = celebritiesCategory.get(randomIndex);
+                    break;
+
+                }
+                while (!guess.equalsIgnoreCase(guessWord) && !outOfGuesses) {
+
+                    if (guessCount < guessLimit) {
+                        System.out.print("Enter a guess: ");
+                        guess = input.nextLine();
+                        guessCount++;
+
+                    }
+
+                    if (guessCount == 2) {
+                        System.out.println("Hint:" + guessWord.charAt(0));
+
+                    }
+
+                    if (guessCount == 5 && !guess.equalsIgnoreCase(guessWord)) {
+                        outOfGuesses = true;
+                        System.out.println("You lose!");
+
+                    }
+                    if(guess.equalsIgnoreCase(guessWord)){
+                        System.out.println("You win!");
+
+                    }
+                }
+                break;
+
+            case 4:
+                //The program will run until the user chooses to exit
+                System.out.println("Goodbye!");
+                System.exit(0);
+
+
         }
-        break;
-         case 4:
-         //The program will run until the user chooses to exit
-         System.out.println("Goodbye!");
-         System.exit(0);
-         }
-         }
     }
+}
