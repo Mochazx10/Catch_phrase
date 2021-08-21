f3f9594dbca3d43b861bdaa98b5dfe9674376928
angeliquegiddings <mochazx10@gmail.com>
2021-08-21 13:52:06 -0400

    update 1

---
+++ src/main/java/Game.java
@@ -0,0 +1,74 @@
+
+import java.util.ArrayList;
+        import java.util.Scanner;
+        import java.util.Random;
+        import java.util.List;
+
+public class Game {
+    public static void main(String[] args) {
+        boolean done = true;
+        boolean yes = true;
+        String operator;
+        //while (done) {
+        System.out.println("Welcome to Catch Phrase! ");
+        System.out.println("-------------------------------");
+        System.out.println("Choose a Category:  ");
+        System.out.println("1: Animal");
+        System.out.println("2: Occupation");
+        System.out.println("3: Celebrity");
+        System.out.println("4: Exit");
+        System.out.println("-------------------------");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        if (x != 4) {
+        }
+  switch (x) {
+        case 1:
+        Random randCategory = new Random();
+        List<String> animalCategory = new ArrayList<String>();
+        animalCategory.add("Lion");
+        animalCategory.add("Elephant");
+        animalCategory.add("Giraffe");
+        int numberOfElements = 1;
+            System.out.println(animalCategory);
+        for (int i = 0; i < numberOfElements; i++) {
+            int randomIndex = randCategory.nextInt(animalCategory.size());
+            String guessWord = animalCategory.get(randomIndex);
+            break;
+        }
+        break;
+        case 2:
+        Random randCategory1 = new Random();
+        List<String> occupationCategory = new ArrayList<String>();
+        occupationCategory.add("Programmer");
+        occupationCategory.add("Lawyer");
+        occupationCategory.add("Doctor");
+        int numberOfElements1 = 1;
+
+        for (int i = 0; i < numberOfElements1; i++) {
+            int randomIndex = randCategory1.nextInt(occupationCategory.size());
+            String guessWord = occupationCategory.get(randomIndex);
+            break;
+        }
+        break;
+        case 3:
+        Random randCategory2 = new Random();
+        List<String> celebritiesCategory = new ArrayList<String>();
+        celebritiesCategory.add("Obama");
+        celebritiesCategory.add("Beyonce");
+        celebritiesCategory.add("Rihanna");
+        int numberOfElements2 = 1;
+
+        for (int i = 0; i < numberOfElements2; i++) {
+            int randomIndex = randCategory2.nextInt(celebritiesCategory.size());
+            String guessWord = celebritiesCategory.get(randomIndex);
+            break;
+        }
+        break;
+         case 4:
+         //The program will run until the user chooses to exit
+         System.out.println("Goodbye!");
+         System.exit(0);
+         }
+         }
+    }
